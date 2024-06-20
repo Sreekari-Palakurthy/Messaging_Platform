478ae637f767f6a25ab85366853aee461de9bae9
sreekari <sreekari.p@gmail.com>
2024-06-20 18:26:43 +0530

    add crud.py file

--- Messaging_app/main.py
+++ Messaging_app/main.py
@@ -4,7 +4,6 @@ import crud, models, schemas, databases
 
 app = FastAPI()
 
-# Dependency
 def get_db():
     db = databases.SessionLocal()
     try:
--- Messaging_app/models.py
+++ Messaging_app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
-from databases import Base
+from databases import Base, engine
 
 class User(Base):
     __tablename__ = "users"
@@ -10,7 +10,6 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     phone_number = Column(String)  # New column for phone number
-
     messages_sent = relationship("Message", foreign_keys="[Message.sender_id]", back_populates="sender")
     messages_received = relationship("Message", foreign_keys="[Message.receiver_id]", back_populates="receiver")
 
@@ -19,18 +18,18 @@ class Thread(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-
     messages = relationship("Message", back_populates="thread")
 
 class Message(Base):
     __tablename__ = "messages"
-
     id = Column(Integer, primary_key=True, index=True)
     sender_id = Column(Integer, ForeignKey('users.id'))
     receiver_id = Column(Integer, ForeignKey('users.id'))
     thread_id = Column(Integer, ForeignKey('threads.id'))
     content = Column(Text, index=True)
-
     sender = relationship("User", foreign_keys=[sender_id], back_populates="messages_sent")
     receiver = relationship("User", foreign_keys=[receiver_id], back_populates="messages_received")
     thread = relationship("Thread", back_populates="messages")
+
+
+Base.metadata.create_all(bind=engine)
