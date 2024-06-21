370ef0cb48d8a962715f533aad20c2b90a1acbe7
sreekari <sreekari.p@gmail.com>
2024-06-21 10:11:33 +0530

    add models.py file

--- Messaging_app/models.py
+++ Messaging_app/models.py
@@ -18,6 +18,7 @@ class Thread(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
+    messages = relationship("Message", back_populates="thread")
     
 
 class Message(Base):
