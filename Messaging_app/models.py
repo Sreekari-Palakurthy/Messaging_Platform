f11705cd0ac950936189669747e6ac9233e8eabb
sreekari <sreekari.p@gmail.com>
2024-06-21 11:38:49 +0530

    add main.py file

--- Messaging_app/models.py
+++ Messaging_app/models.py
@@ -1,36 +1,48 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
-from databases import Base, engine
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from databases import Base, SessionLocal, engine
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    phone_number = Column(String)
-    messages_sent = relationship("Message", foreign_keys="[Message.sender_id]", back_populates="sender")
-    messages_received = relationship("Message", foreign_keys="[Message.receiver_id]", back_populates="receiver")
+    threads = relationship("Thread", back_populates="creator")
+    messages = relationship("Message", back_populates="sender")
 
 class Thread(Base):
-    __tablename__ = "threads"
-
+    __tablename__ = 'threads'
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
+    creator_id = Column(Integer, ForeignKey('users.id'))
+    creator = relationship("User", back_populates="threads")
     messages = relationship("Message", back_populates="thread")
-    
 
 class Message(Base):
-    __tablename__ = "messages"
+    __tablename__ = 'messages'
+
     id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
     sender_id = Column(Integer, ForeignKey('users.id'))
-    receiver_id = Column(Integer, ForeignKey('users.id'))
     thread_id = Column(Integer, ForeignKey('threads.id'))
-    content = Column(Text, index=True)
-    sender = relationship("User", foreign_keys=[sender_id], back_populates="messages_sent")
-    receiver = relationship("User", foreign_keys=[receiver_id], back_populates="messages_received")
+    created_at = Column(DateTime, default=datetime.utcnow)
+    sender = relationship("User", back_populates="messages")
     thread = relationship("Thread", back_populates="messages")
+
 
 
 Base.metadata.create_all(bind=engine)
--- Messaging_app/schemas.py
+++ Messaging_app/schemas.py
@@ -1,11 +1,9 @@
 from pydantic import BaseModel
-from models import DateTime 
-from typing import List
-
 
 class UserBase(BaseModel):
     username: str
     email: str
+    phone_number: int
 
 class UserCreate(UserBase):
     hashed_password: str
@@ -18,27 +16,26 @@ class User(UserBase):
 
 class ThreadBase(BaseModel):
     title: str
+    owner_id: int
 
 class Thread(ThreadBase):
-    creator_id: int
-
-class ThreadInDB(Thread):
     id: int
-    creator: User
-    messages: List["Message"]
 
     class Config:
         orm_mode = True
 
 class MessageBase(BaseModel):
     content: str
+
+class MessageCreate(MessageBase):
     sender_id: int
+    receiver_id: int
     thread_id: int
 
 class Message(MessageBase):
     id: int
-    created_at: DateTime
-    sender: User
+    sender_id: int
+    receiver_id: int
     thread_id: int
 
     class Config:
